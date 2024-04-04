7741f0631232b4b749a3976c74cc284598e81d41
Santino <santiagorvrcn@gmail.com>
2024-04-04 11:49:50 -0500

    add table interactions

--- app/services/[service]/page.jsx
+++ app/services/[service]/page.jsx
@@ -151,18 +151,50 @@ export const Page = () => {
             </div>
 
             <div>
-                <ul>
-                    {newValor.map((element,index) => (
-                        <li key={index}>
-                            <p className='text-white text-1xl'>{element.Month} Semana {element.Week}: {element.Count}</p>
-                        </li>
-                    ))}
-                </ul>
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 rounded-s-lg">
+                                    Mes
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    # de Semana
+                                </th>
+                                <th scope="col" class="px-6 py-3 rounded-e-lg">
+                                    Total
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {newValor.map((element,index) => (
+                                <tr class="bg-white dark:bg-gray-800">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {element.Month}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {element.Week}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {element.Count}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
-        
-        
+
+
+/* <ul>
+    {newValor.map((element,index) => (
+        <li key={index}>
+            <p className='text-white text-1xl'>{element.Month} Semana {element.Week}: {element.Count}</p>
+        </li>
+    ))}
+</ul> */
         
   )
 
