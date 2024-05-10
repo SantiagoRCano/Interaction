10779079a371f811166cd8c7612a5b65e47ea59d
Santino <santiago.cano@cpocket.global>
2024-05-09 23:09:07 -0500

    add users count

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,16 @@
 import React, {useEffect,useState} from "react";
 import { BotonLleva,BotonLlevaTwo } from '@/app/RedirectButton'
 import rakingHttp from '@/hooks/rankingHttp'
+import useHTTP from "@/hooks/useHttp";
 
 
 
 export default function Home() {
   let [rakingData, setRakingData] = useState([])
 
-
   const {rankingData, rakingLoading, rankingError } = rakingHttp(`https://www.cpocketbot.com/api/rankingInteraction`)
+  const { data, loading, error } = useHTTP(`https://www.cpocketbot.com/api/usersPocki`)
+
 
   useEffect(() => {
     if(rankingData && !rakingLoading && !rankingError){
@@ -19,8 +21,16 @@ export default function Home() {
     }
   })
 
+
+
+
   return (
     <main className="flex min-h-[7rem] flex-col items-center justify-center align-middle p-24">
+        {loading && <p>Cargando...</p>}
+        {error && <p>Ocurrió un error al cargar la data.</p>}
+        {data && (
+          <h2 className="text-white mb-6">Cantidad de personas que le han copiado al bot desde que se creo: {data.Total}</h2>
+        )}
         <div className="bg-zander flex flex-wrap justify-center items-center text-center gap-14 p-5 rounded-lg mb-32 h-full">
           <BotonLleva destination={`./services`} buttonText="Mercado"/>
           <BotonLleva destination={`./services`} buttonText="Multas"/>
@@ -35,12 +45,16 @@ export default function Home() {
           <BotonLlevaTwo destination={`./recurrence`} buttonText="Recurrencia"/>
         </div>
 
+        
 
-        <div className="text-center border-green-500 border-2 p-5 rounded-lg">
-          <h1 className="mb-5">Ranking de servicios más usados</h1>
-          {rakingData.map((inter,index)=> (
-            <h3 key={index} className="text-white m-1">{inter.Categoria}: {inter.Total}</h3>
-          ))}
+        <div className="flex justify-between">
+          <div className="text-center border-green-500 border-2 p-5 rounded-lg">
+            <h1 className="mb-5">Ranking de servicios más usados</h1>
+            {loading && <p>Cargando...</p>}
+            {rakingData.map((inter,index)=> (
+              <h3 key={index} className="text-white m-1">{inter.Categoria}: {inter.Total}</h3>
+            ))}
+          </div>
         </div>
     </main>
 
