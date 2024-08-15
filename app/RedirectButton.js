376d838600ea60ac7518b10867e9c5654ef4f91d
Santino <santiago.cano@cpocket.global>
2024-08-15 16:32:42 -0500

    add rank services

--- app/RedirectButton.js
+++ app/RedirectButton.js
@@ -10,7 +10,7 @@ const BotonLleva = ({ destination, buttonText }) => {
 
   return (
     <Link href={`${destination}/${buttonText}`}>
-        <button className='bg-bluesky md:h-[11-vh] md:w-[9vw] p-5 rounded-lg text-white text-center hover:bg-blue-700' onClick={() => updateInfo(buttonText)} >{buttonText}</button>
+        <button className='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full' onClick={() => updateInfo(buttonText)} >{buttonText}</button>
     </Link>
   );
 };
@@ -19,14 +19,23 @@ const BotonLleva = ({ destination, buttonText }) => {
 const BotonLlevaTwo = ({ destination,buttonText }) => {
   return(
     <Link href={destination}>
-      <button className='bg-bluesky md:h-[11-vh] md:w-[9vw] p-5 rounded-lg text-white text-center hover:bg-blue-700'>{buttonText}</button>
+      <button className='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full'>{buttonText}</button>
     </Link>
   )
+}
+
+const BotonLlevaThree = ({ destination,buttonText }) => {
+   return(
+    <Link href={destination}>
+      <button className='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-red-600 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full'>{buttonText}</button>
+    </Link>
+   )
 }
 
 
 
 export {
   BotonLleva,
-  BotonLlevaTwo
+  BotonLlevaTwo,
+  BotonLlevaThree
 };
---
+++ app/rank/page.jsx
@@ -0,0 +1,92 @@
+"use client"
+import React, { useEffect, useState} from 'react'
+import useHTTP from '@/hooks/useHttp'
+
+export const page = () => {
+    let [rankData, setRankData ] = useState([])
+
+    const { data, loading, error } = useHTTP('https://www.cpocketbot.com/api/rankingMonth')
+
+    useEffect(() => {
+        if(data && !loading && !error){
+            setRankData(data)
+        }
+    })
+
+    let formatData = {
+        '2023-May':'Mayo 2023',
+        '2023-June':'Junio 2023',
+        '2023-July':'Julio 2023',
+        '2023-August':'Agosto 2023',
+        '2023-September':'Septiembre 2023',
+        '2023-October':'Octubre 2023',
+        '2023-November':'Noviembre 2023',
+        '2023-December':'Diciembre 2023',
+        '2024-January':'Enero',
+        '2024-February':'Febrero',
+        '2024-March':'Marzo',
+        '2024-April':'Abril',
+        '2024-May':'Mayo',
+        '2024-June':'Junio',
+        '2024-July':'Julio',
+        '2024-August':'Agosto',
+        '2024-September':'Septiembre'
+    };
+
+    const processedData = rankData.map(monthData => {
+        const { Mes, ...services } = monthData;
+        const serviceNames = Object.keys(services);
+        const serviceValues = serviceNames.map(service => parseInt(services[service], 10));
+    
+        const maxServiceValue = Math.max(...serviceValues);
+    
+        const bestService = maxServiceValue === 0 ? "No Servicio" :
+            serviceNames[serviceValues.indexOf(maxServiceValue)].replace("Total", "");
+    
+        return {
+            ...monthData,
+            Mes: formatData[Mes] || Mes,
+            MejorServicio: bestService
+        };
+    });
+
+
+  return (
+    <React.Fragment>
+         <div className="overflow-x-auto p-4">
+            <table className="w-full">
+                <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr className='text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600'>
+                        <th className="px-4 py-3 text-center">Mes</th>
+                        <th className="px-4 py-3 text-center">Multas</th>
+                        <th className="px-4 py-3 text-center">Inmobiliaria</th>
+                        <th className="px-4 py-3 text-center">Mercado</th>
+                        <th className="px-4 py-3 text-center">Imagen</th>
+                        <th className="px-4 py-3 text-center">Recordatorios</th>
+                        <th className="px-4 py-3 text-center">Resumen</th>
+                        <th className="px-4 py-3 text-center">Editar Imagen</th>
+                        <th className="px-4 py-3 text-center">Mejor Servicio</th>
+                    </tr>
+                </thead>
+                <tbody className='bg-white'>
+                    {processedData.map((row, index) => (
+                        <tr key={index} className='bg-green-500'>
+                            <td className="px-4 py-3 border text-center text-white">{row.Mes}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalMultas}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalInmobiliaria}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalMercado}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalImagen}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalRecordatorios}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalResumen}</td>
+                            <td className="px-4 py-3 border text-center text-white">{row.TotalEImagen}</td>
+                            <td className="px-4 py-3 border text-center text-white font-semibold">{row.MejorServicio}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default page;
