b117600017507239059c8043be963dad543019c9
Santino <santiago.cano@cpocket.global>
2024-09-30 11:59:49 -0500

    fix endpoints

--- hooks/otherHttp.js
+++ hooks/otherHttp.js
@@ -1,34 +1,34 @@
 import React from "react";
 import axios from "axios";
 
-
-
 const otherHttp = (url, token) => {
     const [oData, setData] = React.useState(null);
     const [oDataError, setError] = React.useState(null);
     const [oDataLoading, setLoading] = React.useState(true);
 
-
     React.useEffect(() => {
         const fetchData = async () => {
-        setLoading(true);
-        try {
-            const response = await axios.get(url, {
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            });
-            setData(response.data);
-            setError(null);
-        } catch (error) {
-            setError(error);
-        } finally {
-            setLoading(false);
-        }
+            setLoading(true);
+            try {
+                const response = await axios.get(url, {
+                    headers: token ? {
+                        "Authorization": `Bearer ${token}`
+                    } : {}
+                });
+                setData(response.data);
+                setError(null);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setError(error?.response?.data || "Error de conexión");
+            } finally {
+                setLoading(false);
+            }
         };
+        
         fetchData();
-    }, [url]);
+    }, [url, token]);
+
     return { oData, oDataLoading, oDataError };
-}
+};
 
 export default otherHttp;
